746cadb4f8ef5fc01e3276b86dd1115b36bbf2d7
Maaj <maajb1122@gmail.com>
2025-04-12 23:14:27 +0530

    committed

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -2,7 +2,7 @@ const User = require("../models/User");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
-const JWT_SECRET = process.env.JWT_SECRET || "your_jwt_secret"; // Use environment variable
+const JWT_SECRET = process.env.JWT_SECRET || "your_jwt_secret"; 
 
 // Register User
 exports.registerUser = async (req, res) => {
